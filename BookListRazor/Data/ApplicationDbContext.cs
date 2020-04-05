9109aba8ef7ba9345e0f92bc1a4d7d7a9296a086
Arunav <arunavsen96@gmail.com>
2020-04-05 23:23:49 +0600

    Add project files.

---
+++ BookListRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Data;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty] public Book Book { get; set; }
+
+        public async Task <IActionResult> OnGet(int? Id)
+        {
+            Book = new Book();
+
+            //create
+            if (Id==null)
+            {
+                return Page();
+            }
+            //edit
+            Book = await _db.Books.FirstOrDefaultAsync(u => u.Id == Id);
+            if (Book==null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+         {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id==0)
+                {
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+
+        }
+    }
+}
